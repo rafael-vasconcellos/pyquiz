e4ad3ee2b67b3f99fde84645f789c33b943f6f0f
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2023-05-21 08:01:41 -0300

    proto questions

--- questions.py
+++ questions.py
@@ -2,16 +2,41 @@
 
 questions = [
     {
-        "text": "Pergunta",
-        "altA": { "text": "Alternativa A", "label": False, "exp": "Explicação" },
-        "altB": { "text": "Alternativa B", "label": False, "exp": "Explicação" },
-        "altC": { "text": "Alternativa C", "label": True, "exp": "Explicação" },
-        "altD": { "text": "Alternativa D", "label": False, "exp": "Explicação" },
+        "text": "Qual é o maior animal terrestre?",
+        "altA": { "text": "Elefante", "label": True, "exp": "Explicação" },
+        "altB": { "text": "Girafa", "label": False, "exp": "Explicação" },
+        "altC": { "text": "Rinoceronte", "label": False, "exp": "Explicação" },
+        "altD": { "text": "Hipopótamo", "label": False, "exp": "Explicação" },
+        "answered": None
+    },
+
+    {
+        "text": "Qual é o maior animal marinho?",
+        "altA": { "text": "Tubarão-baleia", "label": False, "exp": "Explicação" },
+        "altB": { "text": "Baleia-azul", "label": True, "exp": "Explicação" },
+        "altC": { "text": "Polvo-gigante", "label": False, "exp": "Explicação" },
+        "altD": { "text": "Golfinho", "label": False, "exp": "Explicação" },
+        "answered": None
+    },
+
+    {
+        "text": "Quantas pernas um inseto possui?",
+        "altA": { "text": "2", "label": False, "exp": "Explicação" },
+        "altB": { "text": "4", "label": False, "exp": "Explicação" },
+        "altC": { "text": "6", "label": True, "exp": "Explicação" },
+        "altD": { "text": "8", "label": False, "exp": "Explicação" },
+        "answered": None
+    },
+
+    {
+        "text": "Qual é o maior felino do mundo?",
+        "altA": { "text": "Leão", "label": False, "exp": "Explicação" },
+        "altB": { "text": "Tigre", "label": True, "exp": "Explicação" },
+        "altC": { "text": "Onça-pintada", "label": False, "exp": "Explicação" },
+        "altD": { "text": "Leopardo", "label": False, "exp": "Explicação" },
         "answered": None
     },
 ]
-
-#questions.append(questions[0].copy()) # isto é um teste
 
 class Question:
     def __init__(self, item):
@@ -23,3 +48,20 @@ class Question:
         self.altD = item['altD']
         self.answered = item['answered']
 
+
+
+
+
+"""
+    {
+        "text": "Pergunta",
+        "altA": { "text": "Alternativa A", "label": False, "exp": "Explicação" },
+        "altB": { "text": "Alternativa B", "label": False, "exp": "Explicação" },
+        "altC": { "text": "Alternativa C", "label": True, "exp": "Explicação" },
+        "altD": { "text": "Alternativa D", "label": False, "exp": "Explicação" },
+        "answered": None
+    },
+
+questions.append(questions[0].copy()) # isto é um teste
+
+"""
